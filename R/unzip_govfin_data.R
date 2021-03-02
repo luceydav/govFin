3bae1532b4b433b119df6702d904cb3f86b04cba
luceydav <dnl2001@stern.nyu.edu>
2021-03-02 17:52:12 -0500

    add unzip govfin_data

---
+++ R/unzip_govfin_data.R
@@ -0,0 +1,33 @@
+
+#' Function to extract and rename .csv from downloaded zipped folder
+#'
+#' @description
+#' Unzip folder as .csv but does not extract other documentation files
+#'
+#' @param path Folder in working dir where zip is stored
+#'
+#' @examples
+#' \dontrun{
+#' unzip_govfin_data(path = "data") }
+#'
+#' @export
+unzip_govfin_data <- function(path = "inst/extdata"){
+
+  #https://willamette.edu/mba/research-impact/public-datasets/index.html
+
+  # Set path to zip
+  zip_file <- here::here(path, "gov_fin.zip")
+
+  # Extract csv
+  file <- "The Government Finance Database_All Data.csv"
+  utils::unzip(
+    zip_file,
+    files = file,
+    unzip = getOption("unzip"),
+    exdir = path)
+
+  file.rename(from = paste0(path, "/", list.files(path, pattern=".csv")),
+              to = paste0(path, "/", "gov_fin_data.csv"))
+
+
+}
