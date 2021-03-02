905e0f60864804c0032064d2cbe6b30670e4b45d
luceydav <dnl2001@stern.nyu.edu>
2021-03-02 17:50:15 -0500

    add get_govfin_data

---
+++ R/get_govfin_data.R
@@ -0,0 +1,28 @@
+
+#' Function to download multiple file zipped folder from Willamette
+#'
+#' @description
+#' Downloads full zipped folder from Willamette including data in csv and documentation files
+#'
+#' @param path Folder in working dir to store data
+#'
+#' @examples
+#' \dontrun{
+#' get_govfin_data(path = "data") }
+#'
+#' @export
+get_govfin_data <- function(path = "inst/extdata"){
+
+  #https://willamette.edu/mba/research-impact/public-datasets/index.html
+  url <- "https://willamette.edu/~kpierson/TheGovernmentFinanceDatabase_AllData.zip"
+
+  # Create dir in working dir to store downloaded zip
+  dir.create(here::here(path))
+
+  # Set path to zip
+  zip_file <- here::here(path, "gov_fin.zip")
+
+  # Download file
+  download.file(url, destfile = zip_file)
+
+}
