1e22716b576430ebc50fd1175b2e4e8673e914ba
David Lucey <dnl2001@stern.nyu.edu>
2023-01-21 08:59:42 -0500

    fix params

--- R/full_gov_census_db_wrapper.R
+++ R/full_gov_census_db_wrapper.R
@@ -4,15 +4,19 @@
 #' Runs get_govfin_data(), unzip_govfin_data(), load_and_clean_govcensus_csv(),
 #' then make_db() builds database in datawith make_table()
 #'
+#' @param path Folder in working dir to store data
+#' @param time Timeout set 1000 seconds because default 60 too short
+#' @param connect Type of connection ("duckdb" or "sqlite")
+
 #' @examples
 #' \dontrun{
-#' load_and_clean_govcensus_csv(path = "data")}
+#' full_gov_census_db_wrapper(time = 1000, path = "data")}
 #'
 #' @export
-full_gov_census_db_wrapper <- function(){
-  get_govfin_data()
+full_gov_census_db_wrapper <- function(path = "data", time = 1000, connect = "duckdb"){
+  get_govfin_data(path = path, time = time)
   unzip_govfin_data(path = "data")
   gov_census_list <-
     load_and_clean_govcensus_csv(path = "data")
-  make_db(data = gov_census_list)
+  make_db(data = gov_census_list, connect = "duckdb")
 }
